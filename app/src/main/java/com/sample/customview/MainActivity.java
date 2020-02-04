d04fe580abf5e235a98c5ec638d994e4b84e334a
Mahdizareeii <mahdizareei.hmd@gmail.com>
2020-02-04 14:12:29 +0330

    first commit

--- app/src/main/java/com/sample/customview/MainActivity.java
+++ app/src/main/java/com/sample/customview/MainActivity.java
@@ -3,6 +3,7 @@ package com.sample.customview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.sample.customview.customView.ButtonTextProgress;
 
@@ -14,6 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initAction();
+    }
+
+    private void initView() {
         buttonTextProgress = findViewById(R.id.buttonTextProgress);
+    }
+
+    private void initAction() {
+        /*
+        buttonTextProgress.setBtnConfirmText("Test1");
+        buttonTextProgress.setTxtTitleText("Test2");
+        buttonTextProgress.setProgressBarVisibility(View.GONE);
+        */
     }
 }
